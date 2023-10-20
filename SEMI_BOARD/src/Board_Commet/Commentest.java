aa5c75369c015dc43dbdca3ec7eeb1a3118723fc
LeeKwonHo <yaaa3717@naver.com>
2023-10-20 16:18:19 +0900

    aaaa

---
+++ SEMI_BOARD/src/Board_Commet/Commentest.java
@@ -0,0 +1,54 @@
+package Board_Commet;
+
+public class Commentest {
+
+	public static void main(String[] args) {
+		
+		
+		Comment comment = new Comment();
+//		comment.CommentWrite();
+		
+//		comment.CommentRead();
+		
+		}
+}
+
+		
+//		Connection c = Connect.getConnection();
+//		Scanner sc = new Scanner(System.in);
+//		
+//		try {
+//			if(c != null) {
+//			System.out.println("연결 성공");
+//			}else{
+//			System.out.println("연결 실패");
+//			}
+//			String sql = ""
+//					+ " SELECT * "
+//					+ " FROM boardproject.comment ";
+//
+//			// PreparedStatement 얻기 및 값 지정
+//			PreparedStatement pstmt = c.prepareStatement(sql);
+////			pstmt.setString(1, sc.nextLine());
+//
+//			ResultSet rs = pstmt.executeQuery();
+//			
+//			while (rs.next()) {
+//
+//				System.out.println(rs.getString("context"));
+//			}
+//
+//		} catch (Exception e) {
+//			e.printStackTrace();
+//		} finally {
+//			if (c != null) {
+//				try {
+//					// 연결 끊기
+//					c.close();
+//					System.out.println("연결 끊기");
+//				} catch (SQLException e) {
+//				}
+//			}
+//		}
+
+
