6968e9c89f485b767071e64432105f0db325e344
재원 <asdf>
2023-10-24 10:24:53 +0900

    실패 분기 추가

--- src/boardproject/login.java
+++ src/boardproject/login.java
@@ -5,48 +5,57 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.Scanner;
+import java.sql.Time;
+import java.util.*;
+import java.util.concurrent.TimeUnit;
 
 public class login {
 
    public static boolean loginn = false;
-   public static String nm  ;
-   
-   void log() {
+   public static String nm;
+
+   void log() throws Exception{
 
       Connection conn = Connect.getConnection();
 
       try {
-         
-         
+
          Scanner s = new Scanner(System.in);
 
-               String sql = " SELECT  SEQ " + "        ,ID" + "        ,PW" + "        ,NICKNAME"
-                     + "  FROM member " + " where id = ? ";
-               PreparedStatement pstmt = conn.prepareStatement(sql);
-               System.out.println("아이디 입력");
-               String i = s.nextLine();
-               System.out.println("비밀번호 입력");
-               String p = s.nextLine();
+         while (true) {
+            System.out.println("아이디 입력");
+            String i = s.nextLine();
+            System.out.println("비밀번호 입력");
+            String p = s.nextLine();
+            String sql = " SELECT  SEQ " + "        ,ID" + "        ,PW" + "        ,NICKNAME"
+                  + "  FROM member " + " where id = ? ";
+            PreparedStatement pstmt = conn.prepareStatement(sql);
 
-               pstmt.setString(1, i);
-               ResultSet rs = pstmt.executeQuery();
-               while (rs.next()) { 
-
-                  if (p.equals(rs.getString(3))) {
-                     nm=rs.getString(4);
-                      loginn = true;
-                     System.out.println("로그인성공");
-                  } else {
-                     System.out.println("아이디 또는 비밀번호를 다시 확인해주세요");
-                  }
-               }
+            pstmt.setString(1, i);
+            ResultSet rs = pstmt.executeQuery();
+            if(!rs.next()){
+               System.out.println();
+               System.out.println("존재하지 않는 아이디입니다. 다시 입력해주세요.");
+               System.out.println();
+               continue;
+            }
+            if (p.equals(rs.getString(3))) {
+               nm = rs.getString(4);
+               loginn = true;
+               System.out.println("로그인성공");
+               Thread.sleep(1000);
+               break;
+            } else {
+               System.out.println("로그인 실패: 아이디 또는 비밀번호를 다시 입력해주세요");
+               System.out.println();
+            }
+         }
 
       }
 
       catch (SQLException e) {
          e.printStackTrace();
       }
-         }
-      
    }
+
+}
--- src/boardproject/signup.java
+++ src/boardproject/signup.java
@@ -7,60 +7,65 @@ import java.util.Scanner;
 
 public class signup {
 
-   
-   String singUp() {
-      
+   String singUp() throws Exception {
+
       Connection conn = Connect.getConnection();
       Scanner s = new Scanner(System.in);
 
-      
-      
-      
-      try {
-         String sql = "INSERT INTO  member (" 
-               + ""
-               + "    id" 
-               + "    ,pw" 
-               + "    ,nickname" 
-               + ") VALUES (" 
-               + "    ?"
-               + "    ,?"
-               + "    ,?"
-               + ")";
+      while (true) {
 
-         PreparedStatement pstmt = conn.prepareStatement(sql);
-         System.out.println("***회원가입***");
+         try {
 
-         System.out.println("아이디를 입력하세요");
-         String id = s.nextLine();
-         pstmt.setString(1, id);
+            String sql = "INSERT INTO  member ("
+                  + ""
+                  + "    id"
+                  + "    ,pw"
+                  + "    ,nickname"
+                  + ") VALUES ("
+                  + "    ?"
+                  + "    ,?"
+                  + "    ,?"
+                  + ")";
 
-         System.out.println("비밀번호를 입력하세요");
-         String pw = s.nextLine();
-         pstmt.setString(2, pw);
+            PreparedStatement pstmt = conn.prepareStatement(sql);
+            System.out.println("***회원가입***");
 
-         System.out.println("닉네임을 입력하세요");
-         String nickname = s.nextLine();
-         pstmt.setString(3, nickname);
+            System.out.println("아이디를 입력하세요");
+            String id = s.nextLine();
+            pstmt.setString(1, id);
 
-         int count = pstmt.executeUpdate();
-         if (count == 0) {
-            System.out.println("저장 실패");
-         } else {
-            if (1 < count) {
-               System.out.println("오류");
+            System.out.println("비밀번호를 입력하세요");
+            String pw = s.nextLine();
+            pstmt.setString(2, pw);
+
+            System.out.println("닉네임을 입력하세요");
+            String nickname = s.nextLine();
+            pstmt.setString(3, nickname);
+
+            int count = pstmt.executeUpdate();
+            if (count == 0) {
+               System.out.println("가입 실패");
+               System.out.println("다른 아이디와 닉네임을 선택해주세요.");
+               System.out.println();
             } else {
-               System.out.println("저장 성공 : " + count);
-               return nickname;
-            } 
-         } return "";
-         
-   
-         
-      } catch (SQLException e) {
-      
-         e.printStackTrace();
-         return "";
+               if (1 < count) {
+                  System.out.println("오류");
+               } else {
+                  System.out.println("회원가입 성공");
+                  System.err.println("가입된 정보로 로그인합니다.");
+                  Thread.sleep(1000);
+                  return nickname;
+               }
+            }
+
+         } catch (SQLException e) {
+            System.out.println();
+            System.out.println("가입 실패");
+            System.out.println("다른 아이디와 닉네임을 선택해주세요.");
+            System.out.println();
+            Thread.sleep(1000);
+
+         }
       }
    }
 }
